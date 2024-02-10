b0d8675bbc4571db723e64478d9b1200706a5c33
piet de visser <35933355+pdvmoto@users.noreply.github.com>
2024-02-10 15:46:16 +0100

    docu to add to entrypoints

--- startsardock.sh
+++ startsardock.sh
@@ -3,8 +3,18 @@
 # startsardock.sh : run sar in a docker contaner
 #
 # notes
-# script to (re)start sar data collection when no crontab or system is available.
+# 1, script to (re)start sar data collection when no crontab or system is available.
 # primitive: restart every 10 min to make sure output file is named correctly.
+#
+# 2. add to container-entry, e.g. to /op/oracle/runOracle.sh or other entrypoint
+# 
+# 2.a: get scripts: git clone  https://github.com/pdvmoto/uxutil
+# 2.b: install sar if needed yum install sysstat, verify.
+# 2.c: add to entrypoint:
+# pdv: add running of sar, optional: check files present
+# echo starting sar if possible
+# nohup /opt/div/uxutil/startsardock.sh  &
+# echo started sar if possible > /tmp/sarstart.log
 #
 #
 SARDIR=/var/log/sa/
